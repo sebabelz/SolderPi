3ceaddf81ca4a683ff715bd60d44611e5b9c2e42
Unknown <belz.sebastian@googlemail.com>
2019-03-27 22:05:09 +0100

    read adc after pwm on state

--- stepRespArdu/stepRespArdu.ino
+++ stepRespArdu/stepRespArdu.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
-//#include <I2C_ADC.h>
-#define ADC_ADDR  0x28 
+
+#define ADC_ADDR  0x28
+#define TESTPIN 4
 #define PWMPIN 3
 #define BUTTON 2
 #define PERIOD_TIME 10
@@ -12,32 +13,17 @@ unsigned long onInterval = DUTY_CYCLE * PERIOD_TIME;
 unsigned long offInterval = PERIOD_TIME - onInterval;
 bool offState = true;
 bool enabled = false;
+bool readState = false;
 
 void sensorInit()
 {
   uint8_t configMask = 0x68;       //enable CH1, CH0      B11110000
- 
+
   Wire.begin();
+  Wire.setClock(600000);
   Wire.beginTransmission(ADC_ADDR);
   Wire.write(configMask);
   Wire.endTransmission();
-}
-
-void sensorReadBitWise()
-{
-  uint8_t rxByte[2];
-  Wire.requestFrom(ADC_ADDR, 4);
-
-  for(int i = 0; i < 2; ++i)
-  {
-    rxByte[0] = Wire.read();
-    rxByte[1] = Wire.read();
-
-    print_binary(rxByte[0], 8);
-    Serial.print("\t");
-    print_binary(rxByte[1], 8);
-    Serial.print("\n");
-  }
 }
 
 void sensorRead(uint16_t *data)
@@ -45,49 +31,18 @@ void sensorRead(uint16_t *data)
   uint8_t rxByte[2];
   Wire.requestFrom(ADC_ADDR, 4);
 
-  for(int i = 0; i < 2; ++i)
+  for (int i = 0; i < 2; ++i)
   {
     rxByte[0] = Wire.read();
     rxByte[1] = Wire.read();
-    data[i] = ((rxByte[0]<< 6) & 0x3C0) | ((rxByte[1] >> 2) & 0x3F); 
+    data[i] = ((rxByte[0] << 6) & 0x3C0) | ((rxByte[1] >> 2) & 0x3F);
   }
-}
-
-
-void print_binary(int v, int num_places)
-{
-    int mask=0, n;
-
-    for (n=1; n<=num_places; n++)
-    {
-        mask = (mask << 1) | 0x0001;
-    }
-    v = v & mask;  // truncate v to specified number of places
-
-    while(num_places)
-    {
-
-        if (v & (0x0001 << num_places-1))
-        {
-             Serial.print("1");
-        }
-        else
-        {
-             Serial.print("0");
-        }
-
-        --num_places;
-        if(((num_places%4) == 0) && (num_places != 0))
-        {
-            Serial.print("_");
-        }
-    }
 }
 
 void buttonIntput()
 {
   digitalWrite(PWMPIN, LOW);
-  if(enabled)
+  if (enabled)
   {
     Serial.println("START");
   }
@@ -96,12 +51,13 @@ void buttonIntput()
 
 void testing()
 {
-  Serial.println("test");
+  readState = true;
 }
 
 void setup()
 {
-  Serial.begin(9600);
+  sei();
+  Serial.begin(500000);
   while (!Serial);        // wait for Serial port to connect
   Serial.println("Started");
 
@@ -109,39 +65,32 @@ void setup()
   Serial.println("I2C Initialised");
 
   pinMode(PWMPIN, OUTPUT);
-  TCCR2A = bit(COM2A1) | bit(COM2B1) | bit(WGM21) | bit(WGM20);
-  TCCR2B = bit(WGM22) | bit(CS20);
-  OCR2A = 63;
-  OCR2B = 31;
-  
+  TCCR2A = bit(COM2B1) | bit(WGM20);
+  TCCR2B = bit(WGM22) | bit(CS22);
+  OCR2A = 124;
+  OCR2B = 99;
+
   pinMode(BUTTON, INPUT);
-  
+
+  pinMode(TESTPIN, OUTPUT);
+
   attachInterrupt(digitalPinToInterrupt(BUTTON), buttonIntput , FALLING);
   attachInterrupt(digitalPinToInterrupt(PWMPIN), testing , FALLING);
 }
 
 void loop()
 {
-  if (enabled)
+  if (readState)
   {
-    if (millis() - previousMillis > onInterval && offState == false) 
-    {
-      previousMillis = millis();
-      offState = true;
-      //digitalWrite(PWMPIN, LOW);
-      sensorRead(sensorData);
-      Serial.print(sensorData[0]);
-      Serial.print(", ");
-      Serial.print(sensorData[1]);
-      Serial.print("\n");
-    }
+    digitalWrite(TESTPIN, HIGH);
+    sensorRead(sensorData);
+    digitalWrite(TESTPIN, LOW);
+    Serial.print(sensorData[0]);
+    Serial.print(", ");
+    Serial.print(sensorData[1]);
+    Serial.print("\n");
   
-    if (millis() - previousMillis > offInterval && offState == true) 
-    {
-      previousMillis = millis();
-      offState = false;
-      //digitalWrite(PWMPIN, HIGH);
-
-    }
+    readState = false;
+    
   }
 }
